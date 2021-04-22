3161a8d141afc1564a8555318c0158fe646c0266
Garry Wilson <garry.wilson@imaginecurve.com>
2021-04-22 21:00:05 +0100

    Remove debug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -19,16 +19,9 @@ if [ "$IAM_ROLE" == "none" ]; then
   export AWSACCESSKEYID=${AWSACCESSKEYID:-$AWS_ACCESS_KEY_ID}
   export AWSSECRETACCESSKEY=${AWSSECRETACCESSKEY:-$AWS_SECRET_ACCESS_KEY}
 
-  echo 'IAM_ROLE is not set - /usr/bin/s3fs ${S3_BUCKET} ${MOUNT_POINT} -f -o use_sse,nosuid,nonempty,nodev,allow_other,default_acl=${S3_ACL},retries=5,umask=${UMASK},uid=${UID},gid=${GID},dbglevel=info -o curldbg'
-  /usr/bin/s3fs ${S3_BUCKET} ${MOUNT_POINT} -f -o use_sse,nosuid,nonempty,nodev,allow_other,default_acl=${S3_ACL},retries=5,umask=${UMASK},uid=${UID},gid=${GID},dbglevel=info -o curldbg
+  echo 'IAM_ROLE is not set - /usr/bin/s3fs ${S3_BUCKET} ${MOUNT_POINT} -f -o use_sse,nosuid,nonempty,nodev,allow_other,default_acl=${S3_ACL},retries=5,umask=${UMASK},uid=${UID},gid=${GID}'
+  /usr/bin/s3fs ${S3_BUCKET} ${MOUNT_POINT} -f -o use_sse,nosuid,nonempty,nodev,allow_other,default_acl=${S3_ACL},retries=5,umask=${UMASK},uid=${UID},gid=${GID}
 else
-  echo 'IAM_ROLE is set - /usr/bin/s3fs ${S3_BUCKET} ${MOUNT_POINT} -f -o iam_role=${IAM_ROLE},use_sse,nosuid,nonempty,nodev,allow_other,default_acl=${S3_ACL},retries=5,umask=${UMASK},uid=${UID},gid=${GID},dbglevel=info -o curldbg'
-  /usr/bin/s3fs ${S3_BUCKET} ${MOUNT_POINT} -f -o iam_role=${IAM_ROLE},use_sse,nosuid,nonempty,nodev,allow_other,default_acl=${S3_ACL},retries=5,umask=${UMASK},uid=${UID},gid=${GID},dbglevel=info -o curldbg
-fi
-
-mounted=$(mount | grep s3fs | grep "${MOUNT_POINT}")
-if [ -n "${mounted}" ]; then
-    echo "Mounted bucket ${S3_BUCKET} onto ${MOUNT_POINT}"
-else
-    echo "Mount failure - can't found mount"
+  echo 'IAM_ROLE is set - /usr/bin/s3fs ${S3_BUCKET} ${MOUNT_POINT} -f -o iam_role=${IAM_ROLE},use_sse,nosuid,nonempty,nodev,allow_other,default_acl=${S3_ACL},retries=5,umask=${UMASK},uid=${UID},gid=${GID}'
+  /usr/bin/s3fs ${S3_BUCKET} ${MOUNT_POINT} -f -o iam_role=${IAM_ROLE},use_sse,nosuid,nonempty,nodev,allow_other,default_acl=${S3_ACL},retries=5,umask=${UMASK},uid=${UID},gid=${GID}
 fi
